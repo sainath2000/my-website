006d84186dd8c03a4fbb245a65d2a41a7724f72e
HarshaPeddireddy <harshavardhan833202@gmail.com>
2023-05-13 17:34:51 +0530

    slight changes in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import BrowserRouter from 'react-router-dom'
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
@@ -8,7 +7,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
