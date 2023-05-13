e60f57c6fba49ff811136e6e9b126fdf4e9aced6
HarshaPeddireddy <harshavardhan833202@gmail.com>
2023-05-13 17:31:45 +0530

    slight changes in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import BrowserRouter from 'react-router-dom/BrowserRouter'
+import BrowserRouter from 'react-router-dom'
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
